5db1b2d3bb1940b316bdacf67f6b3ba3589c1fe3
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-10-31 09:36:28 +0100

    Add files via upload

---
+++ exercise7_1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 17:58:33 2017
+
+@author: fxie_
+"""
+
+from mrjob.job import MRJob
+
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class MRWordOccurrences(MRJob):
+
+
+
+    def mapper(self, _, line):
+        # yield each word in the line
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+    
+    def reducer(self, word, counts):
+        # send all (num_occurrences, word) pairs to the same reducer.
+        # num_occurrences is so we can easily use Python's max() function.
+        yield (word,sum(counts))
+
+
+if __name__ == '__main__':
+    MRWordOccurrences.run()
---
+++ exercise7_2.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 29 21:09:12 2017
+
+@author: fxie_
+"""
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+GRAPH_RE = re.compile(r"[\d']+")
+
+class MRMostUsedWord(MRJob):
+    def mapper_get_nodes(self, _, line):
+        # yield each nodes in the line
+        for node in GRAPH_RE.findall((line)):
+#            print("(node{},1)".format(node))
+            print('%s\t%s' % (node, 1))
+            yield (node, 1)
+
+    #def combiner_count_nodes(self, node, counts):
+        # sum the nodes we've seen so far
+    #    yield (node, sum(counts))
+    
+    def reducer_nodes_degree(self, node, counts):
+        # send all (degree, nodes) pairs to the same reducer.
+#        print(node,sum(counts))
+        yield node,sum(counts)
+#       yield None,(sum(counts),node)
+       
+
+        
+
+    # discard the key; it is just None
+    def determine_euler_tour(self,node,degree):
+        # each item of word_count_pairs is (degree, nodes),
+        # so yielding one results in key=counts, value=word
+       for value in degree:
+           
+            if (value%2!=0):
+                yield ('odd',1)
+            else:
+                yield ('even',1)
+#                print('The degree of node{} is NOT even'.format(node)) 
+     
+#                print('The Degree of node{} is even'.format(node))
+    def euler_tour(self,node,degree):
+        print('\n--------------------------------------\n')
+        if (node=='odd'):
+            print('The graph has NOT an Euler tour')
+            
+        else:
+            print('The graph has an Euler Tour')
+        print('--------------------------------------\n')
+
+            
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_nodes,
+#                   combiner=self.combiner_count_nodes,
+#                    reducer_init = self.reduce_init,
+                   reducer=self.reducer_nodes_degree)
+            ,MRStep(
+                   
+                   reducer=self.determine_euler_tour
+                   )
+             ,MRStep(
+                   
+                   reducer=self.reducer_nodes_degree
+                   )
+              ,MRStep(
+                   
+                   reducer=self.euler_tour
+                   )
+        ]
+
+if __name__ == '__main__':
+    MRMostUsedWord.run()
+    
